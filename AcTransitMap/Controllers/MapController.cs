626f12c02578b2fb2ff4487bf628b0cea7095304
Aidan <sir.meepington.1@gmail.com>
2020-12-14 13:56:39 +0000

    Initial commit.

---
+++ AcTransitMap/Controllers/MapController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace AcTransitMap.Controllers
+{
+    public class MapController : Controller
+    {
+        private readonly ILogger<MapController> _logger;
+
+        public MapController(ILogger<MapController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
