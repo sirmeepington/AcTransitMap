fb71226bf182c03c83c45dc8cbc7039b375811ee
Aidan <sir.meepington.1@gmail.com>
2020-12-15 01:29:15 +0000

    Implement map showing bus locations.

---
+++ AcTransitMap/Services/IPositionService.cs
@@ -0,0 +1,12 @@
+﻿using AcTransitMap.Models;
+using GtfsConsumer.Entities.Interfaces;
+using System.Collections.Generic;
+
+namespace AcTransitMap.Services
+{
+    public interface IPositionService
+    {
+        void UpdateVehiclePosition(IVehiclePosition pos);
+        IEnumerable<VehiclePosition> GetPositions();
+    }
+}
---
+++ AcTransitMap/Services/PositionService.cs
@@ -0,0 +1,43 @@
+﻿using AcTransitMap.Models;
+using GtfsConsumer.Entities.Interfaces;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AcTransitMap.Services
+{
+    public class PositionService : IPositionService
+    {
+        private readonly ConcurrentDictionary<string, VehiclePosition> _positions;
+        private readonly ILogger<PositionService> _logger;
+
+        public PositionService(ILogger<PositionService> logger)
+        {
+            _logger = logger;
+            _positions = new ConcurrentDictionary<string, VehiclePosition>();
+        }
+
+        public IEnumerable<VehiclePosition> GetPositions()
+        {
+            return _positions.Values.ToList();
+        }
+
+        public void UpdateVehiclePosition(IVehiclePosition pos)
+        {
+            if (!_positions.TryGetValue(pos.VehicleId, out VehiclePosition vehiclePos))
+            {
+                vehiclePos = new VehiclePosition();
+                vehiclePos.VehicleId = pos.VehicleId;
+            }
+
+            vehiclePos.Latitude = pos.Latitude;
+            vehiclePos.Longitude = pos.Longitude;
+            vehiclePos.LastUpdated = DateTime.UtcNow.ToString("G");
+
+            _positions[pos.VehicleId] = vehiclePos;
+        }
+
+    }
+}
